b8b5073c4a016d727f294923546a69b7d37a203b
chillmelon <chiyuh1996@gmail.com>
2022-10-15 10:22:55 +0800

    add: can now fetch data from api

--- lib/api_utils.rb
+++ lib/api_utils.rb
@@ -6,16 +6,58 @@ module Foodegrient
   class ApiUtils
     API_ROOT = "https://api.spoonacular.com/recipes/findByIngredients"
 
+    module Errors
+      class NotFound < StandardError; end
+      class Unauthorized < StandardError; end
+    end
+
+    HTTP_ERROR = {
+      401 => Errors::Unauthorized,
+      404 => Errors::NotFound
+    }.freeze
+
+    def initialize(token)
+      @api_token = token
+    end
+
     def food_api_path(path)
       "https://api.spoonacular.com/#{path}"
     end
 
-    def call_food_ur(token, url)
-      HTTP.headers('x-api-key' => token).get(url)
+    def call_food_url(url)
+      result = HTTP.headers('x-api-key' => @api_token).get(url)
+      successful?(result) ? result : raise(HTTP_ERROR[result.code])
     end
 
-    food_response = {}
-    food_results = {}
+    def successful?(result)
+      !HTTP_ERROR.keys.include?(result.code)
+    end
+
+    def build_query(ingredients)
+      result = String.new
+      result << 'recipes/findByIngredients'
+      if ingredients.size > 0
+        result << "?ingredients=#{ingredients[0]}"
+        ingredients.shift
+        ingredients.each do |ingredient|
+          result << ",+#{ingredient}"
+        end
+      end
+      puts result
+      result
+    end
+
+    def recipes(ingredients)
+      query = build_query(ingredients)
+      recipes_url = food_api_path(query)
+      puts recipes_url
+      recipes_data = call_food_url(recipes_url).parse
+      recipes_data
+    end
+
+    def save_files(recipes_data)
+      File.write('spec/fixtures/results.yml', recipes_data.to_yaml)
+    end
 
   end
 
