672ad80b80888b11ad75833f7b0e8a32b1ff4dde
WowShiba <oopsdxm@gmail.com>
2022-10-23 05:37:22 +0800

    Fix the path of recipes_results.yml

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -9,7 +9,7 @@ require_relative '../lib/api_utils'
 
 CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 API_TOKEN = CONFIG['FOOD_API_TOKEN']
-CORRECT = YAML.safe_load(File.read('spec/fixtures/recipes_results.yml'))
+CORRECT = YAML.safe_load(File.read('spec/recipes_results.yml'))
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTES_FILE = 'food_api'
 
