64d1cc7e16b55d06d7067dc5bf88f62686bb8c5f
WowShiba <oopsdxm@gmail.com>
2022-10-30 07:45:12 +0800

    Setup the Roda environment

---
+++ app/controllers/app.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'slim'
+
+module Foodegrient
+  # Web App
+  class App < Roda
+    plugin :render, engine: 'slim', views: 'app/views'
+    plugin :assets, css: 'style.css', path: 'app/views/assets'
+    plugin :common_logger, $stderr
+    plugin :halt
+
+    route do |routing|
+      routing.assets # load CSS
+      response['Content-Type'] = 'text/html; charset=utf-8'
+
+      # GET /
+      routing.root do
+        view 'home'
+      end
+
+      routing.on 'result' do
+        # GET /project/owner/project
+        routing.get do
+          view 'result'
+        end
+      end
+    end
+  end
+end
