67ea32368bb46fe23a50638ad17daa342790be92
WowShiba <oopsdxm@gmail.com>
2022-11-18 17:11:23 +0800

    Fix

--- app/domain/menus/mappers/recipe_mapper.rb
+++ app/domain/menus/mappers/recipe_mapper.rb
@@ -3,53 +3,53 @@
 require_relative '../entities/recipe'
 
 module Foodegrient
-    # Provides access to Food data
-    module Spoonacular
-      # Data Mapper: Spoonacular recipes -> Menu entity
-      class RecipeMapper
-        def initialize(data)
-            @data = data
-        end
-  
-        def load_several()
-            @data.map do |item|
-              RecipeMapper.build_entity(item)
-            end
-          end
-    
-          def self.build_entity(data)
-            DataMapper.new(data).build_entity
-          end
-    
-          # Extracts entity specific elements from data structure
-          class DataMapper
-            def initialize(data)
-              @data = data
-            end
-    
-            def build_entity
-              Entity::Recipe.new(
-                id: nil,
-                image:,
-                title:,
-                # ingredients:
-              )
-            end
-    
-            private
-    
-            def id
-                @data['id']
-              end
+  # Provides access to Food data
+  module Spoonacular
+    # Data Mapper: Spoonacular recipes -> Menu entity
+    class RecipeMapper
+      def initialize(data)
+        @data = data
+      end
 
-            def image
-              @data['image']
-            end
-    
-            def title
-              @data['title']
-            end
-          end
+      def load_several()
+        @data.map do |item|
+          RecipeMapper.build_entity(item)
+        end
+      end
+
+      def self.build_entity(data)
+        DataMapper.new(data).build_entity
+      end
+
+      # Extracts entity specific elements from data structure
+      class DataMapper
+        def initialize(data)
+          @data = data
+        end
+
+        def build_entity
+          Entity::Recipe.new(
+            id: nil,
+            image:,
+            title:
+            # ingredients:
+          )
+        end
+
+        private
+
+        def id
+          @data['id']
+        end
+
+        def image
+          @data['image']
+        end
+
+        def title
+          @data['title']
+        end
       end
     end
+  end
 end
